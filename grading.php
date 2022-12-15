533ddaee04d554188fb68d4e0a84ae7c8867ca0d
Zarin-Tasnim-Roshni <zarin.roshni@northsouth.edu>
2022-12-16 04:09:35 +0600

    grading policy implemented

--- grading.php
+++ grading.php
@@ -10,26 +10,53 @@ else{
     echo "Database connected". "<br>". "<br>";
 }
 
-$course = $_POST["id"];
-$pass = $_POST["password"];
 
+$course = $_POST["course"];
+$id = $_POST["id"];
+$quiz = $_POST["quiz"];
+$mid = $_POST["mid"];
+$final = $_POST["final"];
+
+
+echo "Course: ", $course."<br>";
 echo "Student ID: ", $id."<br>";
-echo "Student pass: ", $pass."<br>";
+echo "quiz: ", $quiz."<br>";
+echo "mid: ", $mid."<br>";
+echo "final: ", $final."<br>";
 
-$sql = "SELECT PASS FROM reg_info WHERE ID = $id";
+$sql = "SELECT Course_Name FROM course WHERE  Student_ID = $id";
 $result = $db->query($sql);
+
 
 $row = $result->fetch_assoc();
 
-if($row["PASS"]==$pass){
+echo "course name : ", $row["Course_Name"];
+
+if($row["Course_Name"]==$course){
+   
     
-    echo"successfull";
+
+    $result=$quiz+$mid+$final;
+
+    switch($result) {
+        case $result >= 70 and $result <= 100:
+            echo 'Your Grade is A';
+            break;
+        case $result >= 60 and $result <= 69:
+            echo 'Your Grade is B+';
+            break;
+        case $result >= 59 and $result <= 60:
+            echo 'Your Grade is B';
+            break;
+        default:
+        echo 'something else';
+        }
+
 
 }
 
 else{
     
-    echo"error";
-
+ echo "error";
+ 
 }
-?>
