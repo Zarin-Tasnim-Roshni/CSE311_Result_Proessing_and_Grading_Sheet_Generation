b989fc86fae84e8b2e82480f94371ed706d4fb7c
Rafin14 <rafin.hassan@northsouth.edu>
2022-12-18 03:59:35 +0600

    updatedcourse reg

--- course_reg.php
+++ course_reg.php
@@ -14,14 +14,14 @@
 	if(isset($_POST['submit']))
 	{
 		// Store the Product name in a "name" variable
-		$name = mysqli_real_escape_string($con,$_POST['Course_Name']);
+		$name = mysqli_real_escape_string($db,$_POST['Course_Name']);
 		
 		// Store the Category ID in a "id" variable
-		$id = mysqli_real_escape_string($con,$_POST['Teacher_Initial']);
+		$id = mysqli_real_escape_string($db,$_POST['Teacher_Initial']);
 		
 		// Creating an insert query using SQL syntax and
 		// storing it in a variable.
-		$sql_insert =
+		/*$sql_insert =
 		"INSERT INTO student_course(Course_Name, Student_ID)
 			VALUES ('$name','$id')";
 		
@@ -29,10 +29,11 @@
 		// if the query executes with no errors
 		// a javascript alert message is displayed
 		// which says the data is inserted successfully
-		if(mysqli_query($con,$sql_insert))
+		if(mysqli_query($db,$sql_insert))
 		{
 			echo '<script>alert("Course added successfully")</script>';
 		}
+        */
 	}
 ?>
 
@@ -46,9 +47,9 @@
 		content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-	<form method="POST">
+	<form action = "course_reg2.php" method="POST">
 
-		<label>Select a Category</label>
+		<label>Select a Course</label>
 
 		<select name="Category">
 			<?php
@@ -71,7 +72,7 @@
 			?>
 		</select>
 		<br>
-		<input type="submit" value="submit" name="submit">
+		<input type="submit" value="submit" >
 	</form>
 	<br>
 </body>
---
+++ course_reg2.php
@@ -0,0 +1,43 @@
+<?php
+
+$db = mysqli_connect("localhost", "root", "", "result_processing");
+
+if(!$db){
+
+    echo "Database not connected";
+}
+else{ 
+    echo "Database connected". "<br>". "<br>";
+}
+
+$st_name = $_POST["name"];
+$id = $_POST["id"];
+$st_email = $_POST["email"];
+$st_gender = $_POST["gender"];
+$pass = $_POST["password"];
+
+$sql = "INSERT INTO student_info(StudentName,StudentEmail,Gender,password,Student_ID) VALUES('$st_name','$st_email','$st_gender','$pass',$id)";
+
+echo "Student Name: ", $st_name."<br>";
+echo "Student ID: ", $id."<br>";
+echo "Student Pass: ", $pass."<br>";
+echo "Student email: ", $st_email."<br>";
+echo "Student gender: ", $st_gender."<br>";
+
+
+if(mysqli_query($db, $sql)){
+
+    echo "Information Inserted". "<br>". "<br>";
+    $sql="INSERT INTO result(Student_ID) VALUES($id)";
+    mysqli_query($db, $sql);
+
+     echo "Information Inserted". "<br>". "<br>";
+
+}
+else{
+
+    echo "error" . mysqli_error($db);
+}
+
+
+?>
