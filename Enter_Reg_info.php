27b336a060646ff6beb487dd1953cb8840b40b55
Rafin14 <rafin.hassan@northsouth.edu>
2022-12-18 03:46:41 +0600

    drop down menu added

--- Enter_Reg_info.php
+++ Enter_Reg_info.php
@@ -14,25 +14,21 @@ $st_name = $_POST["name"];
 $id = $_POST["id"];
 $st_email = $_POST["email"];
 $st_gender = $_POST["gender"];
-$dob = $_POST["dob"];
-$st_course = $_POST["course_name"];
 $pass = $_POST["password"];
 
-$sql = "INSERT INTO student_info(StudentName,StudentEmail,Gender,DOB,Course_Name,password,Student_ID) VALUES('$st_name','$st_email','$st_gender', $dob, '$st_course','$pass',$id)";
+$sql = "INSERT INTO student_info(StudentName,StudentEmail,Gender,password,Student_ID) VALUES('$st_name','$st_email','$st_gender','$pass',$id)";
 
 echo "Student Name: ", $st_name."<br>";
 echo "Student ID: ", $id."<br>";
 echo "Student Pass: ", $pass."<br>";
 echo "Student email: ", $st_email."<br>";
 echo "Student gender: ", $st_gender."<br>";
-echo "Student course: ", $st_course."<br>";
-echo "Student dob: ", $dob."<br>";
 
 
 if(mysqli_query($db, $sql)){
 
     echo "Information Inserted". "<br>". "<br>";
-    $sql="INSERT INTO course(Course_Name,Student_ID) VALUES('$st_course',$id)";
+    $sql="INSERT INTO result(Student_ID) VALUES($id)";
     mysqli_query($db, $sql);
 
      echo "Information Inserted". "<br>". "<br>";
