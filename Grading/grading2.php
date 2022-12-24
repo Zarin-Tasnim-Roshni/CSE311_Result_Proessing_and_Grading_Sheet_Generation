c08233ad714b003472054f63ff04471a024051ea
Zarin-Tasnim-Roshni <zarin.roshni@northsouth.edu>
2022-12-24 12:32:47 +0600

    Welcome html modified, grading2 php modified

--- Grading/grading2.php
+++ Grading/grading2.php
@@ -7,13 +7,13 @@ $course = $_SESSION["t_coursename"];
 
 $db = mysqli_connect("localhost", "root", "", "result_processing");
 
-if(!$db){
+/*if(!$db){
 
     echo "Database not connected";
 }
 else{ 
     echo "Database connected". "<br>". "<br>";
-}
+}*/
 
 $id = $_POST["id"];
 $quiz = $_POST["quiz"];
@@ -21,10 +21,10 @@ $mid = $_POST["mid"];
 $final = $_POST["final"];
 
 
-echo "Student ID: ", $id."<br>";
+/*echo "Student ID: ", $id."<br>";
 echo "quiz: ", $quiz."<br>";
 echo "mid: ", $mid."<br>";
-echo "final: ", $final."<br>";
+echo "final: ", $final."<br>";*/
 
 $sql = "SELECT Course_Name FROM result WHERE  Student_ID = $id AND Course_Name = '$course'";
 $result = $db->query($sql);
@@ -32,7 +32,7 @@ $result = $db->query($sql);
 
 $row = $result->fetch_assoc();
 
-echo "course name : ", $row["Course_Name"];
+/*echo "course name : ", $row["Course_Name"];*/
 
 
 if($row["Course_Name"]==$course){
@@ -41,9 +41,10 @@ if($row["Course_Name"]==$course){
     $result=$quiz+$mid+$final;
 
     switch($result) {
-        case $result >= 70 and $result <= 100:
+        case $result >= 80 and $result <= 100:
 
-            echo 'Your Grade is A';
+            echo "<h1>"."Your submitted grade is A. Please press back to submit more grades"."</h1>";
+            
             $grade = 'A';
 
             $sql= "UPDATE result SET QUIZ = $quiz WHERE Student_ID = $id AND Course_Name = '$course'";
@@ -61,9 +62,9 @@ if($row["Course_Name"]==$course){
             break;
 
 
-        case $result >= 60 and $result <= 69:
+        case $result >= 70 and $result <= 79:
 
-            echo 'Your Grade is B+';
+            echo "<h1>"."Your submitted grade is B. Please press back to submit more grades"."</h1>";
             $grade = 'B';
 
             $sql= "UPDATE result SET QUIZ = $quiz WHERE Student_ID = $id AND Course_Name = '$course'";
@@ -80,9 +81,9 @@ if($row["Course_Name"]==$course){
             
             break;
 
-        case $result >= 59 and $result <= 60:
+        case $result >= 60 and $result <= 69:
 
-            echo 'Your Grade is B';
+            echo "<h1>"."Your submitted grade is C. Please press back to submit more grades"."</h1>";
             $grade = 'C';
             
             $sql= "UPDATE result SET QUIZ = $quiz WHERE Student_ID = $id AND Course_Name = '$course'";
@@ -97,8 +98,47 @@ if($row["Course_Name"]==$course){
             $sql= "UPDATE result SET GRADE = '$grade' WHERE Student_ID = $id AND Course_Name = '$course'";
             mysqli_query($db, $sql);
 
+            break;
+
+        case $result >= 50 and $result <= 59:
+
+            echo "<h1>"."Your submitted grade is D. Please press back to submit more grades"."</h1>";
+                $grade = 'D';
+                
+                $sql= "UPDATE result SET QUIZ = $quiz WHERE Student_ID = $id AND Course_Name = '$course'";
+                mysqli_query($db, $sql);
+    
+                $sql= "UPDATE result SET MID = $mid WHERE Student_ID = $id AND Course_Name = '$course'";
+                mysqli_query($db, $sql);
+    
+                $sql= "UPDATE result SET FINAL = $final WHERE Student_ID = $id AND Course_Name = '$course'";
+                mysqli_query($db, $sql);
+    
+                $sql= "UPDATE result SET GRADE = '$grade' WHERE Student_ID = $id AND Course_Name = '$course'";
+                mysqli_query($db, $sql);
+
+                break;
+    
+
+                case $result >= 0 and $result <= 49:
+
+                    echo "<h1>"."Your submitted grade is F. Please press back to submit more grades"."</h1>";
+                        $grade = 'F';
+                        
+                        $sql= "UPDATE result SET QUIZ = $quiz WHERE Student_ID = $id AND Course_Name = '$course'";
+                        mysqli_query($db, $sql);
             
-        default:
+                        $sql= "UPDATE result SET MID = $mid WHERE Student_ID = $id AND Course_Name = '$course'";
+                        mysqli_query($db, $sql);
+            
+                        $sql= "UPDATE result SET FINAL = $final WHERE Student_ID = $id AND Course_Name = '$course'";
+                        mysqli_query($db, $sql);
+            
+                        $sql= "UPDATE result SET GRADE = '$grade' WHERE Student_ID = $id AND Course_Name = '$course'";
+                        mysqli_query($db, $sql);
+    
+                      break;
+                    default:
         echo 'something else';
         }
 
